319cca5c02abd89d508ae6e360ca9e9761c8db39
rayanalysis <ray.analysis@gmail.com>
2023-04-29 22:06:24 -0600

    Updating brdf_lut_calculator.

--- complexpbr/brdf_lut_calculator.py
+++ complexpbr/brdf_lut_calculator.py
@@ -13,6 +13,7 @@ def compile_shader(source, shader_type):
     shader = glCreateShader(shader_type)
     glShaderSource(shader, source)
     glCompileShader(shader)
+    print('Compiling BRDF LUT shader...')
     return shader
 
 def create_program(vertex_shader, fragment_shader):
@@ -20,6 +21,7 @@ def create_program(vertex_shader, fragment_shader):
     glAttachShader(program, vertex_shader)
     glAttachShader(program, fragment_shader)
     glLinkProgram(program)
+    print('Creating BRDF LUT program...')
     return program
 
 vertex_shader_src = '''
@@ -40,6 +42,9 @@ fragment_shader_src = '''
 #version 330 core
 out vec4 FragColor;
 in vec2 TexCoord;
+
+uniform sampler2D inputTexture;
+uniform float aspectRatio;
 
 const float PI = 3.14159265359;
 
@@ -80,20 +85,25 @@ vec3 ImportanceSampleGGX(vec2 Xi, vec3 N, float roughness)
 }
 
 void main()
-{		
-    vec2 uv = TexCoord;
+{       
+    vec2 uv = TexCoord * vec2(1.0, aspectRatio);
     vec3 N = vec3(0.0, 0.0, 1.0);
     vec3 V = vec3(sqrt(1.0 - uv.y) * cos(2.0 * PI * uv.x), sqrt(1.0 - uv.y) * sin(2.0 * PI * uv.x), uv.y);
     
+    vec4 inputColor = texture(inputTexture, TexCoord);
+
+    float roughness = inputColor.r;
+    uint SAMPLE_COUNT = uint(1024u * max(inputColor.g, 0.001));
+    
     float A = 0.0;
     float B = 0.0;
-    
-    uint SAMPLE_COUNT = 1024u;
+
     for(uint i = 0u; i < SAMPLE_COUNT; ++i)
     {
         vec2 Xi = Hammersley(i, SAMPLE_COUNT);
-        vec3 H = ImportanceSampleGGX(Xi, N, uv.x);
+        vec3 H = ImportanceSampleGGX(Xi, N, roughness);
         vec3 L = 2.0 * dot(V, H) * H - V;
+
         
         float NdotL = max(L.z, 0.0);
         float NdotV = max(V.z, 0.0);
@@ -109,7 +119,7 @@ void main()
             A += (1.0 - Fc) * G_Vis;
             B += Fc * G_Vis;
         }
-    }
+}
     A /= float(SAMPLE_COUNT);
     B /= float(SAMPLE_COUNT);
     
@@ -117,90 +127,109 @@ void main()
 }
 '''
 
-# Initialize GLUT
-glutInit()
-glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-glutInitWindowSize(512, 512)
-glutCreateWindow(b'BRDF LUT Generator')
+def capture_lut():
+    # initialize GLUT
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(512, 512)
+    glutCreateWindow(b'BRDF LUT Generator')
 
-# Compile shaders and create a program
-vertex_shader = compile_shader(vertex_shader_src, GL_VERTEX_SHADER)
-fragment_shader = compile_shader(fragment_shader_src, GL_FRAGMENT_SHADER)
-shader_program = create_program(vertex_shader, fragment_shader)
+    # compile shaders and create a program
+    vertex_shader = compile_shader(vertex_shader_src, GL_VERTEX_SHADER)
+    fragment_shader = compile_shader(fragment_shader_src, GL_FRAGMENT_SHADER)
+    shader_program = create_program(vertex_shader, fragment_shader)
 
-# Generate a quad to render the texture
-quad_vertices = np.array([
-    -1.0, 1.0, 0.0, 0.0, 1.0,
-    -1.0, -1.0, 0.0, 0.0, 0.0,
-    1.0, 1.0, 0.0, 1.0, 1.0,
-    1.0, -1.0, 0.0, 1.0, 0.0
-], dtype=np.float32)
+    # generate a quad to render the texture
+    quad_vertices = np.array([
+        -1.0, 1.0, 0.0, 0.0, 1.0,
+        -1.0, -1.0, 0.0, 0.0, 0.0,
+        1.0, 1.0, 0.0, 1.0, 1.0,
+        1.0, -1.0, 0.0, 1.0, 0.0
+    ], dtype=np.float32)
 
-vertex_array_object = glGenVertexArrays(1)
-glBindVertexArray(vertex_array_object)
+    vertex_array_object = glGenVertexArrays(1)
+    glBindVertexArray(vertex_array_object)
 
-vertex_buffer_object = glGenBuffers(1)
-glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object)
-glBufferData(GL_ARRAY_BUFFER, quad_vertices, GL_STATIC_DRAW)
+    vertex_buffer_object = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object)
+    glBufferData(GL_ARRAY_BUFFER, quad_vertices, GL_STATIC_DRAW)
 
-glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, None)
-glEnableVertexAttribArray(1)
-glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, None)
+    glEnableVertexAttribArray(1)
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
 
-glUseProgram(shader_program)
+    glUseProgram(shader_program)
 
-# Load the input texture
-input_image = Image.open('input_texture.png')  # provide some precaptured framebuffer texture from your program/game here
-input_image_data = np.array(input_image, dtype=np.uint8)
+    # load the input texture
+    input_image = Image.open('input_texture.png')  # provide some precaptured framebuffer texture from your program/game here
+    input_image_data = np.array(input_image, dtype=np.uint8)
 
-input_texture = glGenTextures(1)
-glBindTexture(GL_TEXTURE_2D, input_texture)
-glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, input_image.width, input_image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, input_image_data)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    # set a fixed size for the output texture
+    output_texture_size = 512
+    
+    # calculate the aspect ratio
+    aspect_ratio = float(input_image.width) / float(input_image.height)
 
-# Set the input texture as a uniform in the shader
-glUniform1i(glGetUniformLocation(shader_program, "inputTexture"), 0)
+    input_texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, input_texture)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, input_image.width, input_image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, input_image_data)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
 
-# Generate framebuffer to render the output texture
-framebuffer = glGenFramebuffers(1)
-glBindFramebuffer(GL_FRAMEBUFFER, framebuffer)
+    # set the input texture as a uniform in the shader
+    glUniform1i(glGetUniformLocation(shader_program, "inputTexture"), 0)
 
-# Generate the output texture
-output_texture = glGenTextures(1)
-glBindTexture(GL_TEXTURE_2D, output_texture)
-glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, input_image.width, input_image.height, 0, GL_RGBA, GL_FLOAT, None)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    # pass the aspect ratio to the shader
+    glUniform1f(glGetUniformLocation(shader_program, "aspectRatio"), aspect_ratio)
 
-# Attach the output texture to the framebuffer
-glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, output_texture, 0)
+    # generate framebuffer to render the output texture
+    framebuffer = glGenFramebuffers(1)
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer)
 
-# Render the output texture
-glViewport(0, 0, input_image.width, input_image.height)
-glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+    # generate the output texture
+    output_texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, output_texture)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, output_texture_size, output_texture_size, 0, GL_RGBA, GL_FLOAT, None)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
 
-# Read the output texture data
-output_texture_data = glReadPixels(0, 0, input_image.width, input_image.height, GL_RGBA, GL_FLOAT)
-output_texture_data = np.frombuffer(output_texture_data, dtype=np.float32).reshape((input_image.height, input_image.width, 4))
+    # attach the output texture to the framebuffer
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, output_texture, 0)
 
-# Save the output texture to a file
-output_texture_image = (output_texture_data * 65535).astype(np.uint16)  # Convert to 16-bit
-Image.fromarray(output_texture_image, mode='RGBA').save('output_brdf_lut.png')
+    # bind the input texture to a texture unit and set the uniform sampler2D in the shader
+    glActiveTexture(GL_TEXTURE0)
+    glBindTexture(GL_TEXTURE_2D, input_texture)
+    glUniform1i(glGetUniformLocation(shader_program, "inputTexture"), 0)
 
-# Clean up
-glDeleteTextures(2, [input_texture, output_texture])
-glDeleteFramebuffers(1, [framebuffer])
-glDeleteVertexArrays(1, [vertex_array_object])
-glDeleteBuffers(1, [vertex_buffer_object])
-glDeleteProgram(shader_program)
-glDeleteShader(vertex_shader)
-glDeleteShader(fragment_shader)
-glutDestroyWindow(glutGetWindow())
+    # render the output texture
+    glViewport(0, 0, output_texture_size, output_texture_size)
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
 
-print("Output texture generated and saved as output_brdf_lut.png")
+    # read the output texture data
+    output_texture_data = glReadPixels(0, 0, output_texture_size, output_texture_size, GL_RGBA, GL_FLOAT)
+    output_texture_data = np.frombuffer(output_texture_data, dtype=np.float32).reshape((output_texture_size, output_texture_size, 4))
+
+    # save the output texture to a file
+    output_texture_image = (np.nan_to_num(output_texture_data) * 65535).astype(np.uint16)  # convert to 16-bit
+    Image.fromarray(output_texture_image, mode='RGBA').save('output_brdf_lut.png')
+    print('Saved BRDF LUT image...')
+    # return Image.fromarray(output_texture_image, mode='RGBA')
+
+    # clean up
+    glDeleteTextures(2, [input_texture, output_texture])
+    glDeleteFramebuffers(1, [framebuffer])
+    glDeleteVertexArrays(1, [vertex_array_object])
+    glDeleteBuffers(1, [vertex_buffer_object])
+    glDeleteProgram(shader_program)
+    glDeleteShader(vertex_shader)
+    glDeleteShader(fragment_shader)
+    glutDestroyWindow(glutGetWindow())
+
+    print("Output texture generated and saved as output_brdf_lut.png")
+
+capture_lut()
