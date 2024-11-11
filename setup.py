8231eac979c8e8232c053f546023de72ec0d7391
rayanalysis <ray.analysis@gmail.com>
2024-11-10 22:01:17 -0500

    Updating version number to 0.5.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='panda3d-complexpbr',
-    version='0.5.9',
+    version='0.5.8',
     packages=['complexpbr'],
     package_data={
        "": ["*.txt","*.vert","*.frag","*.png"],
